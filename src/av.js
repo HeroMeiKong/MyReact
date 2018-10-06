d25ad3880079b9b17265aece9807929ad5082372
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-06 21:25:04 +0800

    test leancloud

--- src/av.js
+++
@@ -1,9 +0,0 @@
-import AV from 'leancloud-storage'
-
-var APP_ID = 'ATwdiNu4opHiuaLvHh4RX6Sy-gzGzoHsz';
-var APP_KEY = 'n224PeuEeiaquD6PJi6bhpme';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
