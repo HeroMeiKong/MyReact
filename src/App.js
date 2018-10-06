dc0c236511b0faf3a4a08b035b44fc6e5bbe1833
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-06 15:31:31 +0800

    add todo

--- src/App.js
+++ src/App.js
@@ -9,32 +9,45 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
-      todoList: [
-        {id:1,title:'第一个待办'},
-        {id:2,title:'第二个待办'}
-      ]
+      newTodo: '',
+      todoList: []
     }
   }
   render() {
     let todos = this.state.todoList.map((item,index)=>{
       return (
-        <li><TodoItem todo={item} /></li>
+        <li key={index}><TodoItem todo={item} /></li>
       )
     })
+    console.log(todos)
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>{todos}</ol>
       </div>
     )
   }
-  addTodo(){
-    console.log('add a todo')
+  addTodo(event){
+    this.state.todolist.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
   }
 }
 
 export default App;
+
+let id = 0
+function idMaker(){
+  id += 1
+  return id
+}
