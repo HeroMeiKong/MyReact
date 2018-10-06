cf042660f6e497f29a481467e0a16e769f28e360
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-06 15:39:03 +0800

    修复value，使用onChange方法

--- src/App.js
+++ src/App.js
@@ -19,19 +19,26 @@ class App extends Component {
         <li key={index}><TodoItem todo={item} /></li>
       )
     })
-    console.log(todos)
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+          <TodoInput content={this.state.newTodo} 
+          onChange={this.changeTitle.bind(this)}
+          onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol>{todos}</ol>
       </div>
     )
   }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
   addTodo(event){
-    this.state.todolist.push({
+    this.state.todoList.push({
       id: idMaker(),
       title: event.target.value,
       status: null,
