70d2d0bb712a97a1267d62856e78dda231147b6d
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-07 16:41:05 +0800

    将部分组件改成函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,19 +1,17 @@
-import React,{Component} from 'react'
+import React from 'react'
 import './TodoInput.css'
 
-export default class TodoInput extends Component{
-  render(){
-    return <input type="text" defaultValue={this.props.content}
+export default function (props){
+  return <input type="text" value={props.content}
     className="TodoInput"
-    onChange={this.changeTitle.bind(this)}
-    onKeyPress={this.submit.bind(this)} />
+    onChange={changeTitle.bind(null,props)}
+    onKeyPress={submit.bind(null,props)} />
+}
+function submit(props,e){
+  if(e.key === 'Enter'){
+    props.onSubmit(e)
   }
-  submit(e){
-    if(e.key === 'Enter'){
-      this.props.onSubmit(e)
-    }
-  }
-  changeTitle(e){
-    this.props.onChange(e)
-  }
+}
+function changeTitle(props,e){
+  props.onChange(e)
 }
