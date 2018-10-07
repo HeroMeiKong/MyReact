c7f542a25db2965df46e040d73ace33680d46d38
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-07 11:27:53 +0800

    合并 onSignUp 和 onSignIn

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
           onSubmit={this.addTodo.bind(this)} />
         </div>
         <ol className="todoList">{todos}</ol>
-        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUp.bind(this)} onSignIn={this.onSignIn.bind(this)} />}
+        {this.state.user.id ? null : <UserDialog onSignUp={this.onSignUpOrSignIn.bind(this)} onSignIn={this.onSignUpOrSignIn.bind(this)} />}
       </div>
     )
   }
@@ -47,12 +47,7 @@ class App extends Component {
     stateCopy.user = {}
     this.setState(stateCopy)
   }
-  onSignUp(user){
-    let stateCopy = JSON.parse(JSON.stringify(this.state))
-    stateCopy.user = user
-    this.setState(stateCopy)
-  }
-  onSignIn(user){
+  onSignUpOrSignIn(user){
     let stateCopy = JSON.parse(JSON.stringify(this.state))
     stateCopy.user = user
     this.setState(stateCopy)
