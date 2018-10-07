255c39c4d88a989bb589865434ec25d09c7d399c
xiaopang <xiaopangfeng4@hotmail.com>
2018-10-07 18:06:53 +0800

    修改bug及更改destroy功能

--- src/LeanCloud.js
+++ src/LeanCloud.js
@@ -13,6 +13,7 @@ export default AV
 export const TodoModel = {
   getByUser(user,successFn,errorFn){
     let query = new AV.Query('Todo')
+    query.equalTo('deleted',false)
     query.find().then((response)=>{
       let array = response.map((t)=>{
         return {id: t.id, ...t.attributes}
@@ -31,6 +32,7 @@ export const TodoModel = {
     let acl = new AV.ACL()
     acl.setPublicReadAccess(false)
     acl.setWriteAccess(AV.User.current(),true)
+    acl.setReadAccess(AV.User.current(),true)
     todo.setACL(acl)
     todo.save().then(function (response) {
       successFn.call(null,response.id)
@@ -50,12 +52,7 @@ export const TodoModel = {
     })
   },
   destroy(todoId,successFn,errorFn){
-    let todo = AV.Object.createWithoutData('Todo',todoId)
-    todo.destroy().then(function(response){
-      successFn && successFn.call(null)
-    },function(error){
-      errorFn && errorFn.call(null,error)
-    })
+    TodoModel.update({id: todoId,deleted: true},successFn,errorFn)
   }
 }
 export function signUp(username,password,email,successFn,errorFn){
